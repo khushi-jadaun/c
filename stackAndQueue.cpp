7fd31c6341591ae09d4fade9187754304379e111
khushi-jadaun <118794480+khushi-jadaun@users.noreply.github.com>
2025-12-28 17:45:12 +0530

    .

--- stackAndQueue.cpp
+++ stackAndQueue.cpp
@@ -416,3 +416,202 @@
 //     cout << postfixToPrefix(s);
 //     return 0;
 // }
+
+// prefix to postfix conversion
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// string prefixToPostfix(string s)
+// {
+//     stack<string> st;
+//     string x;
+//     reverse(s.begin(), s.end());
+//     for (char ch : s)
+//     {
+//         if (isalnum(ch))
+//         {
+//             st.push(string(1, ch));
+//         }
+//         else
+//         {
+//             string t1 = st.top();
+//             st.pop();
+//             string t2 = st.top();
+//             st.pop();
+//             x = t1 + t2 + ch;
+//             st.push(x);
+//         }
+//     }
+//     return st.top();
+// }
+
+// int main()
+// {
+//     string s;
+//     cin >> s;
+//     cout << postfixToPrefix(s);
+//     return 0;
+// }
+
+// next greater element
+// brute froce approach
+
+// #include <iostream>
+// #include <vector>
+// using namespace std;
+
+// vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+//     vector<int> ans;
+
+//     for (int i = 0; i < nums1.size(); i++) {
+//         int j;
+//         for (j = 0; j < nums2.size(); j++) {
+//             if (nums1[i] == nums2[j])
+//                 break;
+//         }
+
+//         int nextGreater = -1;
+//         for (int k = j + 1; k < nums2.size(); k++) {
+//             if (nums2[k] > nums1[i]) {
+//                 nextGreater = nums2[k];
+//                 break;
+//             }
+//         }
+
+//         ans.push_back(nextGreater);
+//     }
+
+//     return ans;
+// }
+
+// int main() {
+//     vector<int> nums1 = {4,1,2};
+//     vector<int> nums2 = {1,3,4,2};
+
+//     vector<int> res = nextGreaterElement(nums1, nums2);
+//     for (int x : res)
+//         cout << x << " ";
+// }
+
+// optimized approach
+
+// #include <iostream>
+// #include <vector>
+// #include <stack>
+// #include <unordered_map>
+// using namespace std;
+
+// vector<int> nextGreaterElement(vector<int> &nums1, vector<int> &nums2)
+// {
+//     unordered_map<int, int> mp; // stores next greater for nums2
+//     stack<int> st;
+
+//     // Step 1: Process nums2
+//     for (int num : nums2)
+//     {
+//         while (!st.empty() && st.top() < num)
+//         {
+//             mp[st.top()] = num;
+//             st.pop();
+//         }
+//         st.push(num);
+//     }
+
+//     // Step 2: Remaining elements have no greater element
+//     while (!st.empty())
+//     {
+//         mp[st.top()] = -1;
+//         st.pop();
+//     }
+
+//     // Step 3: Build answer for nums1
+//     vector<int> ans;
+//     for (int num : nums1)
+//     {
+//         ans.push_back(mp[num]);
+//     }
+
+//     return ans;
+// }
+
+// int main()
+// {
+//     vector<int> nums1 = {4, 1, 2};
+//     vector<int> nums2 = {1, 3, 4, 2};
+
+//     vector<int> res = nextGreaterElement(nums1, nums2);
+//     for (int x : res)
+//         cout << x << " ";
+// }
+
+// Trapping Rainwater
+
+// brute force approach
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int trap(int n, vector<int> &arr, vector<int> &prefix, vector<int> &suffix)
+// {
+//     int total = 0;
+//     for (int i = 1; i < n - 1; i++)
+//     {
+//         int water = min(prefix[i], suffix[i]) - arr[i];
+//         if (water > 0)
+//         {
+//             total += water;
+//         }
+//     }
+//     return total;
+// }
+
+// int main()
+// {
+//     vector<int> arr = {0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
+//     int n = arr.size();
+
+//     vector<int> prefix(n);
+//     prefix[0] = arr[0];
+//     for (int i = 1; i < n - 1; i++)
+//     {
+//         prefix[i] = max(prefix[i - 1], arr[i]);
+//     }
+//     vector<int> suffix(n);
+//     suffix[n - 1] = arr[n - 1];
+//     for (int i = n - 2; i >= 0; i--)
+//     {
+//         suffix[i] = max(suffix[i + 1], arr[i]);
+//     }
+//     cout << trap(n, arr, prefix, suffix);
+// }
+
+// previous Smaller element
+
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> nse()
+{
+    vector<int> arr = {2, 4, 3, 8, 7, 6, 5, 9};
+    stack<int> st;
+    vector<int> nse;
+    for (int i = 0; i < arr.size(); i++)
+    {
+        while (!st.empty() && st.top() >= arr[i])
+        {
+            st.pop();
+        }
+        nse.push_back(st.empty() ? -1 : st.top());
+        st.push(arr[i]);
+    }
+    return nse;
+}
+
+int main()
+{
+    vector<int> ans = nse();
+    for (int i : ans)
+    {
+        cout << i << "  ";
+    }
+}
