bcb97ae64b44ae0ee3b189b4cadde4e9e6a9e3c3
khushi-jadaun <118794480+khushi-jadaun@users.noreply.github.com>
2025-12-30 19:17:01 +0530

    trees

---
+++ tree.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val)
+    {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+void preorder(Node *root)
+{
+    if (root == NULL)
+        return;
+    cout << root->data << " ";
+    preorder(root->left);
+    preorder(root->right);
+}
+
+int main()
+{
+    struct Node *root = new Node(10);
+    root->left = new Node(5);
+    root->right = new Node(15);
+    root->right->left = new Node(20);
+
+    preorder(root);
+}
